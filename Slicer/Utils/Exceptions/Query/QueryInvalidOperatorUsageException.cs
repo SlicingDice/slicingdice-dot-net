74bb3168d442d5c0c263492cf12e7f04f5391826
Wesley W Werneck <wesley@simbioseventures.com>
2017-01-03 19:48:08 +0000

    Add new exceptions classes

---
+++ Slicer/Utils/Exceptions/Query/QueryInvalidOperatorUsageException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slicer.Utils.Exceptions
+{
+    public class QueryInvalidOperatorUsageException : SlicingDiceException
+    {
+        public QueryInvalidOperatorUsageException()
+            : base() { }
+
+        public QueryInvalidOperatorUsageException(string message)
+            : base(message) { }
+
+        public QueryInvalidOperatorUsageException(string format, params object[] args)
+            : base(string.Format(format, args)) { }
+
+    }
+}
---
+++ Slicer/Utils/Exceptions/Request/RequestInvalidHttpMethodException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slicer.Utils.Exceptions
+{
+    public class RequestInvalidHttpMethodException : SlicingDiceException
+    {
+        public RequestInvalidHttpMethodException()
+            : base() { }
+
+        public RequestInvalidHttpMethodException(string message)
+            : base(message) { }
+
+        public RequestInvalidHttpMethodException(string format, params object[] args)
+            : base(string.Format(format, args)) { }
+
+    }
+}
