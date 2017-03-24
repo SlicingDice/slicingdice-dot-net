521c46547abdce1e60df95935e193a021910f176
João Eduardo Medeiros <joao@simbioseventures.com>
2017-03-24 15:55:13 -0300

    Correct error code

--- Slicer/Core/HandlerResponse.cs
+++ Slicer/Core/HandlerResponse.cs
@@ -70,7 +70,7 @@ namespace Slicer.Core
                     throw new AccountBannedException(message);
                 case 33:
                     throw new AccountDisabledException(message);
-                case 30:
+                case 39:
                     throw new IndexInvalidRangeException(message);   
                 // Field errors (40 - 59)
                 case 40:
