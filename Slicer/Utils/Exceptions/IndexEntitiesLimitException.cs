57c17a086600ef7e91d01812b535dca1b9490c17
Rafael Telles <rafaelt@simbioseventures.com>
2017-05-10 19:16:56 -0300

    Update API errors

---
+++ Slicer/Utils/Exceptions/IndexEntitiesLimitException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slicer.Utils.Exceptions
+{
+    class IndexEntitiesLimitException : SlicingDiceException
+    {
+        public IndexEntitiesLimitException()
+            : base() { }
+
+        public IndexEntitiesLimitException(string message)
+			: base(message) { }
+
+		public IndexEntitiesLimitException(Dictionary<string, dynamic> data)
+			: base(data) { }
+
+        public IndexEntitiesLimitException(string format, params object[] args)
+            : base(string.Format(format, args)) { }
+
+    }
+}
